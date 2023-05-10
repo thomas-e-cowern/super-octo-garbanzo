a17adc1e1c1d4364daae349983e2fca7e53f55a3
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-05-10 14:15:57 -0400

    update alignment in _buildContent

--- lib/App/sign_in/sign_in_page.dart
+++ lib/App/sign_in/sign_in_page.dart
@@ -8,31 +8,38 @@ class SignInPage extends StatelessWidget {
         title: const Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.orange,
-              child: const SizedBox(
-                height: 100.0,
-              ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: const SizedBox(
+              height: 100.0,
             ),
-            Container(
-              color: Colors.red,
-              child: const SizedBox(
-                height: 100.0,
-              ),
+          ),
+          const SizedBox(height: 8.0,),
+          Container(
+            color: Colors.red,
+            child: const SizedBox(
+              height: 100.0,
             ),
-            Container(
-              color: Colors.purple,
-              child: const SizedBox(
-                height: 100.0,
-              ),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(height: 8.0,),
+          Container(
+            color: Colors.purple,
+            child: const SizedBox(
+              height: 100.0,
+            ),
+          )
+        ],
       ),
     );
   }
